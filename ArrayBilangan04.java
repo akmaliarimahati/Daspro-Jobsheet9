e49a676b0d9237c808da176866c9920d37c3febe
akmaliarimahati <akmaliarimahati09@gmail.com>
2025-11-21 07:58:07 +0700

    Modifikasi

--- ArrayBilangan04.java
+++ ArrayBilangan04.java
@@ -1,12 +1,20 @@
 public class ArrayBilangan04 {
     public static void main(String[] args) {
 
-        int [] bil = new int[4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        // int [] bil = new int[4];
+        // bil [0] = 5;
+        // bil [1] = 13;
+        // bil [2] = -7;
+        // bil [3] = 17;
 
+        // int [] bil = new int[4];
+        // bil [0] = 5.0;
+        // bil [1] = 12867;
+        // bil [2] = 7.5;
+        // bil [3] = 2000000;
+        
+        int [] bil = {5 , 13 , -7 , 17 };
+        
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
